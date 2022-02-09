442c4c1b336d22041b8b518ebe4a75522a624e09
PiekharievaK <pechareva@gmail.com>
2022-02-09 23:22:58 +0200

    Восстанавливает App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,39 @@
+import Statistics from './statistics/Statistics';
+import Profile from './profile/Profile';
+import FriendList from './friendList/FriendList';
+import TransactionHistory from './transactionHistory/TransactionHistory';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friendList from '../data/friends.json';
+import transactions from '../data/transactions.json';
 export const App = () => {
   return (
-    <div
+    <div 
       style={{
-        height: '100vh',
+      //  height: '100px',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
+        // fontSize: 40,
+        // textTransform: 'uppercase',
         color: '#010101',
       }}
     >
-      React homework template
+      <Profile className='profile'
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friendList} />
+      
+      <TransactionHistory items={transactions} />
+
     </div>
   );
 };
